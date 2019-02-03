5c328cd86642f8c35d6e3ac72b569702e26644b7
wuguan <wuguan@pinduoduo.com>
2019-02-03 19:48:07 +0800

    threesumclosest

---
+++ src/main/java/com/bleachyiqihu/javapractice/leetcode/threesumclosest/Main.java
@@ -0,0 +1,9 @@
+package com.bleachyiqihu.javapractice.leetcode.threesumclosest;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{0, 2, 4, 6, 7};
+        System.out.println(solution.threeSumClosest(nums, 3));
+    }
+}
